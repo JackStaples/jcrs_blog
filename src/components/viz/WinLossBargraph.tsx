3b427dd0602117cca0959f7e7af81525bd90b747
Jack Staples <JackStaplesjcrs@gmail.com>
2022-11-22 10:59:23 -0800

    Cleanup to functions

--- src/components/viz/WinLossBargraph.tsx
+++ src/components/viz/WinLossBargraph.tsx
@@ -7,82 +7,97 @@ interface Props {
     winColour: Colour,
     lossColour: Colour,
     barHeight: number,
-    spacing: number,
-    axisLines: number,
+    axisLinesInterval: number,
 }
 
-export const WinLossBarchart = ({ data, winColour, lossColour, barHeight, spacing, axisLines }: Props) => {
-    const strokeWidth = 0.25;
-    let curY = strokeWidth + barHeight;
-    const width = 100;
-    const height = ( barHeight + spacing ) * data.length + curY
-    const mostWinsOrLosses = data.reduce<number>((result, item) => {
+const strokeWidth = 0.25;
+const width = 100;
+
+function getMaximumWinsOrLosses(data: { wins: number, losses: number }[]) {
+    return data.reduce<number>((result, item) => {
         const sum = item.wins > item.losses ? item.wins : item.losses;
         return sum > result ? sum : result;
-    }, 0)
-    const axisLinesCount = Math.round((mostWinsOrLosses * 2) / axisLines);
-    const axisText: string[] = [];
-    for (let i = mostWinsOrLosses; i >= 0; i-=axisLines) {
-        axisText.push(String(i));
+    }, 0);
+}
+
+function generateWinLossAxisLabels(maximum: number, increment: number): string[] {
+    const retVal: string[] = [];
+
+    for (let i = maximum; i >= 0; i -= increment) {
+        retVal.push(String(i));
     }
-    console.log(axisText)
+    console.log(retVal)
+    return retVal;
+}
+
+export const WinLossBarchart = ({ data, winColour, lossColour, barHeight, axisLinesInterval }: Props) => {
+    const spaceBetweenBars = barHeight / 3;
+    let curY = strokeWidth + barHeight + spaceBetweenBars;
+    const height = (barHeight + spaceBetweenBars) * data.length + curY
+
+    const mostWinsOrLosses = getMaximumWinsOrLosses(data);
+    const axisText: string[] = generateWinLossAxisLabels(mostWinsOrLosses, axisLinesInterval);
+
+    const numberOfAxisLines = Math.round((mostWinsOrLosses * 2) / axisLinesInterval);
+
     return (
         <svg className={bargraph} viewBox={`0 0 ${width} ${height}`}>
-            <rect 
+            <rect
                 x={0}
                 y={barHeight}
-                height={height - barHeight} 
-                width={width} 
-                stroke='black' 
+                height={height - barHeight}
+                width={width}
+                stroke='black'
                 fillOpacity={0}
                 strokeWidth={strokeWidth}
             />
-            {Array.apply(0, Array(axisLinesCount + 1)).map((j, i) => {
-                const x = i * (width/axisLinesCount);
-                const num = axisText[i] ? 
+            {Array.apply(0, Array(numberOfAxisLines + 1)).map((j, i) => {
+                const x = i * (width / numberOfAxisLines);
+                const num = axisText[i] ?
                     axisText[i] :
-                    axisLinesCount % 2 === 1 ?
+                    numberOfAxisLines % 2 === 1 ?
                         axisText[(axisText.length - 1) - (i - axisText.length)] :
                         axisText[(axisText.length - 2) - (i - axisText.length)];
-                
+
                 return (
                     <>
-                        <text 
-                            y={barHeight} 
+                        <text
+                            y={barHeight}
                             x={x - 1}
-                            style={{'fontSize': `${barHeight * 0.75}`}}>
-                                {`${num}`}
+                            style={{ fontSize: `${barHeight * 0.75}px` }}
+                        >
+                            {`${num}`}
                         </text>
-                        <line 
-                            x1={x} 
-                            x2={x} 
-                            y1={barHeight} 
-                            y2={height} 
-                            stroke='black' 
-                            strokeWidth={0.25}
+                        <line
+                            x1={x}
+                            x2={x}
+                            y1={barHeight}
+                            y2={height}
+                            stroke='black'
+                            strokeWidth={strokeWidth}
                             strokeOpacity={0.25} />
                     </>
                 )
-                
+
             })}
             {data.map((el) => {
                 const winBarWidth = (el.wins / mostWinsOrLosses) * (width / 2) - strokeWidth
                 const lossBarWidth = (el.losses / mostWinsOrLosses) * (width / 2) - strokeWidth
                 const y = curY;
-                curY += barHeight + spacing;
+                curY += barHeight + spaceBetweenBars;
                 return (
                     <>
-                        <rect 
-                            fill={winColour} 
-                            y={y} 
+                        <rect
+                            fill={winColour}
+                            y={y}
                             x={width / 2}
-                            width={winBarWidth} 
+                            width={winBarWidth}
                             height={barHeight} />
-                        <rect 
-                            fill={lossColour} 
-                            y={y} 
-                            x={width / 2 - lossBarWidth} 
-                            width={lossBarWidth} 
+                        <rect
+                            fill={lossColour}
+                            y={y}
+                            x={width / 2 - lossBarWidth}
+                            width={lossBarWidth}
                             height={barHeight} />
                     </>
                 )
