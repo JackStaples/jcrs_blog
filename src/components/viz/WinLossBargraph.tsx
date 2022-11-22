eb0b203b7ab10e3a481e797202a2c061bb4e8daa
jackstaples <jackstaples@uvic.ca>
2022-11-21 19:58:45 -0800

    Change scaling to viewBox

--- src/components/viz/WinLossBargraph.tsx
+++ src/components/viz/WinLossBargraph.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { bargraph } from './WinLossBargraph.module.css'
 import type { Colour } from '../../types/Colour'
 
 interface Props {
@@ -6,19 +7,18 @@ interface Props {
     winColour: Colour,
     lossColour: Colour,
     barHeight: number,
-    width: number,
     spacing: number,
 }
 
-export const WinLossBarchart = ({ data, winColour, lossColour, barHeight, width, spacing }: Props) => {
+export const WinLossBarchart = ({ data, winColour, lossColour, barHeight, spacing }: Props) => {
     let curY = 0;
-
+    const width = 100;
     const mostWinsOrLosses = data.reduce<number>((result, item) => {
         const sum = item.wins > item.losses ? item.wins : item.losses;
         return sum > result ? sum : result;
     }, 0)
     return (
-        <svg width={width}>
+        <svg className={bargraph} viewBox='0 0 100 100'>
             {data.map((el) => {
                 const winBarWidth = (el.wins / mostWinsOrLosses) * (width / 2)
                 const lossBarWidth = (el.losses / mostWinsOrLosses) * (width / 2)
@@ -26,14 +26,30 @@ export const WinLossBarchart = ({ data, winColour, lossColour, barHeight, width,
                 curY += barHeight + spacing;
                 return (
                     <>
-                        <rect fill={winColour} y={y} x={width / 2} width={winBarWidth} height={barHeight}></rect>
-                        <foreignObject x={width / 2} y={y} width={winBarWidth} height={barHeight}>
-                            <p>{el.wins}</p>
-                        </foreignObject>
-                        <rect fill={lossColour} y={y} x={width / 2 - lossBarWidth} width={lossBarWidth} height={barHeight}></rect>
-                        <foreignObject y={y} x={width / 2 - lossBarWidth} width={lossBarWidth} height={barHeight}>
-                            <p>{el.losses}</p>
-                        </foreignObject>
+                        <rect 
+                            fill={winColour} 
+                            y={y} 
+                            x={width / 2}
+                            width={winBarWidth} 
+                            height={barHeight} />
+                        <text 
+                            y={y + (barHeight * 0.75)} 
+                            x={(width / 2) + (winBarWidth - (barHeight + 0.25))} 
+                            style={{'fontSize': `${barHeight}`}}>
+                            {el.wins}
+                        </text>
+                        <rect 
+                            fill={lossColour} 
+                            y={y} 
+                            x={width / 2 - lossBarWidth} 
+                            width={lossBarWidth} 
+                            height={barHeight} />
+                        <text 
+                            y={y + (barHeight * 0.75)} 
+                            x={(width / 2) - (lossBarWidth - 0.25)} 
+                            style={{'fontSize': `${barHeight}`}}>
+                            {el.losses}
+                        </text>
                     </>
                 )
             })}
