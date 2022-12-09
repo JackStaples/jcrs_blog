57bc7e55115f72ebe8ad58a091a27a3751157b4f
Jack Staples <JackStaplesjcrs@gmail.com>
2022-12-08 18:26:44 -0800

    Add scaling

--- src/components/viz/Bargraph/Bargraph.tsx
+++ src/components/viz/Bargraph/Bargraph.tsx
@@ -1,27 +1,32 @@
 import React from "react";
 import { Bar } from "./Bar";
 
-export const Bargraph: React.FC<{ data: { length: number, isSelected: boolean }[] }> = ({
+export const Bargraph: React.FC<{
+  scale: (val: number) => number,
+  data: { value: number, isSelected?: boolean }[]
+}> = ({
+  scale,
   data,
 }) => {
-  const spacer = 15 / data.length;
-  const height = 90 / data.length - spacer;
-  return (
-    <>
-      <svg viewBox="0 0 100 100">
-        {data.map((el, i) => {
-          return (
-            <Bar
-              key={height * i}
-              x={0}
-              y={(height + spacer) * i + 10}
-              height={height}
-              width={el.length}
-              colour={el.isSelected ? "blue" : "gray"}
-            />
-          )
-        })}
-      </svg>
-    </>
-  );
-};
+    const spacer = 15 / data.length;
+    const height = 90 / data.length - spacer;
+    return (
+      <>
+        <svg viewBox="0 0 100 100">
+          <line x1={0} x2={100} y1={9} y2={9} stroke="black" strokeWidth={0.2} />
+          {data.map((el, i) => {
+            return (
+              <Bar
+                key={height * i}
+                x={0}
+                y={(height + spacer) * i + 10 + (spacer / 2)}
+                height={height}
+                width={scale(el.value)}
+                colour={el.isSelected ? "#2AA9D5" : "#c8c2ba"}
+              />
+            )
+          })}
+        </svg>
+      </>
+    );
+  };
--- src/components/viz/Bargraph/BargraphComponent.tsx
+++ src/components/viz/Bargraph/BargraphComponent.tsx
@@ -2,13 +2,16 @@ import React, { useState } from "react";
 import { Bargraph } from "./Bargraph";
 
 export const BargraphComponent = () => {
-  const [data, setData] = useState([{ length: 25 }, { length: 55 }, { length: 35 }, { length: 92 }, { length: 36 }]);
+  const [data, setData] = useState([{ value: 25 }, { value: 55, isSelected: true }, { value: 35 }, { value: 92 }, { value: 36 }]);
+  const scale = (value: number) => {
+    return value;
+  }
   return <>
-    <Bargraph data={data} />
+    <Bargraph data={data} scale={scale} />
     <button onClick={() => {
       const newData = [];
       for (const el of data) {
-        newData.push({ length: el.length - 10 });
+        newData.push({ value: el.value - 10 });
       }
       setData(newData);
     }}>Click Me
