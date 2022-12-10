ed93388df97f2e88eedcf622df7ad309dd46f811
jackstaples <jackstaples@uvic.ca>
2022-12-09 19:24:23 -0800

    Add selection

--- src/components/viz/Bargraph/Bar.tsx
+++ src/components/viz/Bargraph/Bar.tsx
@@ -1,37 +1,70 @@
 import React, { useRef, useEffect, useState } from "react";
 
 interface props {
-    x: number,
-    y: number,
-    height: number,
-    width: number,
-    colour: string
-    hoverColour?: string,
+  x: number;
+  y: number;
+  height: number;
+  width: number;
+  colour: string;
+  hoverColour?: string;
+  selectedColour?: string;
 }
 
-export const Bar: React.FC<props> = ({ x, y, height, width, colour, hoverColour }) => {
-    const [isHovered, setIsHovered] = useState(false);
-    const prevWidth = useRef(0)
-    const rectProps = {
-        x,
-        y,
-        height,
-        width: 0,
-        fill: colour,
+export const Bar: React.FC<props> = ({
+  x,
+  y,
+  height,
+  width,
+  colour,
+  hoverColour,
+  selectedColour,
+}) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isSelected, setIsSelected] = useState(false);
+  const prevWidth = useRef(0);
+  const rectProps: React.SVGProps<SVGRectElement> = {
+    x,
+    y,
+    height,
+    width: 0,
+    fill: isHovered || isSelected ? hoverColour : colour,
+  };
+  if (hoverColour) {
+    rectProps.onMouseEnter = () => {
+      setIsHovered(true);
+    };
+    rectProps.onMouseLeave = () => {
+      setIsHovered(false);
+    };
+  }
+  if (isSelected) {
+    rectProps.stroke = selectedColour;
+    rectProps.strokeWidth = "0.5";
+  }
+  if (selectedColour) {
+    rectProps.onClick = () => {
+      setIsSelected(!isSelected);
+    };
+  }
+  let animationRef: SVGAnimateElement;
+  useEffect(() => {
+    prevWidth.current = width;
+    if (animationRef) {
+      animationRef.beginElement();
     }
-    if (hoverColour) {
-
-    }
-    let animationRef: SVGAnimateElement;
-    useEffect(() => {
-        prevWidth.current = width;
-        if (animationRef) {
-            animationRef.beginElement();
-        }
-    }, [width, colour])
-    return <rect
-        {...rectProps}
-    >
-        <animate attributeName="width" from={`${prevWidth.current}`} to={width} dur="0.5s" fill="freeze" ref={(animation: SVGAnimateElement) => { animationRef = animation }} />
+  }, [width, colour]);
+  return (
+    <rect {...rectProps}>
+      <animate
+        attributeName="width"
+        from={`${prevWidth.current}`}
+        to={width}
+        dur="0.5s"
+        fill="freeze"
+        ref={(animation: SVGAnimateElement) => {
+          animationRef = animation;
+        }}
+      />
     </rect>
-}
+  );
+};
